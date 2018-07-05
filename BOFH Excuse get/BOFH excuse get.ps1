ea6ad2542770274918f948bad03715dc1ed247eb
kramit <mike@michaelwhitehouse.com>
2018-07-05 21:41:33 +0100

    Updates

---
+++ BOFH Excuse get/BOFH excuse get.ps1
@@ -0,0 +1,6 @@
+﻿$page = Invoke-WebRequest -Uri http://pages.cs.wisc.edu/~ballard/bofh/bofhserver.pl -Method Get
+$line = $page.ParsedHtml.getElementsByTagName('font') | Select-Object -ExpandProperty innertext
+$excuse = $line[1]
+Write-Host "Todays excuse is" 
+write-host "$excuse"
+
---
+++ Zendesk API/zendesk api email get.ps1
@@ -0,0 +1,27 @@
+﻿$username = ""
+
+$token = ""
+
+
+for ($i = 0; $i -lt 1; $i++)
+{ 
+    
+
+$params = @{
+
+Uri = " https://godeploy.zendesk.com/api/v2/tickets.json";
+
+Method = 'Get';
+
+Headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("$($username):$($token)"));
+
+} #end headers hash table
+
+} #end $params hash table
+
+$result = Invoke-RestMethod -Uri $params.Uri -Method $params.Method -Headers $params.Headers
+$emails += $result.users.email
+
+}
+
+#$emails | Measure-Object
