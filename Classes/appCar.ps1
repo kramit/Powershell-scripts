de11c7478b09c688948271fa65f5816f1e4e97d0
kramit <mike@michaelwhitehouse.com>
2019-08-18 23:19:42 +0100

    fixing driver side method

--- Classes/appCar.ps1
+++ Classes/appCar.ps1
@@ -1,16 +1,16 @@
 Import-Module $PSScriptRoot\carClass.ps1
 
-
 # Using Static "new" method
-$redSportsCar = [car]::new("100","red")
+$redSportsCar = [car]::new("100","red","US")
 
 # Using New-Object. Parameters for Argument list are positional and required by the constructor.
-$blueSportsCar = New-Object car -ArgumentList "100", "red"
+$blueSportsCar = New-Object car -ArgumentList "100", "red","UK"
 
 # Using a HashTable. Note: requires default or parameterless constructor.
 $greenSportsCar = [car]@{
     topSpeed = "100"
     colour = "green"
+    country = "UK"
 }
 
 # Dynamic Object Type using a variable name.
--- Classes/carClass.ps1
+++ Classes/carClass.ps1
@@ -3,13 +3,13 @@ class car
     #Props
     [Int32] $topSpeed
     [string] $colour
+    [string] $country
     
     # Static Props
     Static [string] $UK = "Right"
     Static [string] $US = "Left"
 
     # Hidden Props
-
     Hidden [string] $cO2Output
 
     #paramless constructor
@@ -19,10 +19,11 @@ class car
     }
 
     #constructor
-    car ([int32]$topSpeed,[string]$colour)
+    car ([int32]$topSpeed,[string]$colour,[string]$country)
     {
         $this.topSpeed = $topSpeed
         $this.colour = $colour
+        $this.country = $country
     }
 
     # Method
@@ -30,13 +31,18 @@ class car
     [String] addTurbo()
     {
         $this.topSpeed = $this.topSpeed + (($this.topSpeed / 100)*10)
-
         return "boost added top speed is now :"+$this.topSpeed     
     }
 
-    #Static method
-    Static [string] getTopSpeed()
+    #Method
+    [string] getDriverSide()
     {
-        return [car]::topspeed
+        $side=$null
+        if ($this.country -eq "US"){
+            $side = "left"
+        } elseif ($this.country -eq "UK") {
+            $side = "right"
+        }
+        return $side
     }
 }
