6cb0155563bb1c3b562847f6c4cb8122ebcacd09
kramit <mike@michaelwhitehouse.com>
2019-12-06 18:18:00 +0000

    Update DotNET48downloader.ps1

--- Software Downloaders/DotNET48downloader.ps1
+++ Software Downloaders/DotNET48downloader.ps1
@@ -1,10 +1,10 @@
 
 
-# Set file and folder path for SSMS installer .exe
+# Set file and folder path for .NET installer .exe
 $folderpath="$env:TEMP"
 $filepath="$folderpath\ndp48-x86-x64-allos-enu.exe"
  
-#If SSMS not present, download
+#If .NET not present, download
 if (!(Test-Path $filepath)){
 write-host "Downloading .NET 4.8..."
 $URL = "https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe"
@@ -18,7 +18,7 @@ else {
 write-host "Located the .NET Installer binaries, moving on to install..."
 }
  
-# start the SSMS installer
+# start the .NET installer
 write-host "Beginning .NET 4.8 install..." -nonewline
 $Parms = " /q /Norestart /Logs log.txt"
 $Prms = $Parms.Split(" ")
